02f8f7218940347e6944ad4134bc78ee7d7e32fe
Naveen-Saravanan-01 <saravanannaveen26@gmail.com>
2025-02-14 13:14:18 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import './App.css'
 const App = () => {
   const [joke, setJoke] = useState("");
 
+  const [loading,setLoading]=useState(false)
+
   const fetchJoke = async () => {
+    setLoading(true)
+
+
     try {
       const response = await fetch("https://joke-app-021d.onrender.com/"); // Fetch from our backend API
       const data = await response.json();
       setJoke(data.joke);
     } catch (error) {
       console.error("Error fetching joke:", error);
+    }finally{
+      setLoading(false)
     }
   };
 
@@ -23,10 +30,26 @@ const App = () => {
     <div className="app">
     <div className="joke-container" >
       <h1>Random Joke Generator</h1>
-      <div className="joke">
+
+      {loading?
+
+      (<div className="loading">
+        <div className="spinner"></div>
+        <p>Loading...</p>
+      </div>) :
+
+
+
+
+
+      (<div className="joke">
         <p>{joke || "Loading joke..."}</p>
-      </div>
+     
       <button onClick={fetchJoke}>Get New Joke</button>
+      </div>
+    )
+    
+  }
     </div>
 
     </div>
